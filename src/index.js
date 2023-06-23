579c610038ed9c3e3059e13804afe856d80d6b8f
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-23 15:17:17 -0700

    WIP html form

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import person from './../src/galaxy.js';
 
+window.addEventListener("load", function (){
+  document.querySelector("form#person-info").addEventListener("submit", handleFormSubmission);
+});
+
 function handleFormSubmission(event) {
   event.preventDefault();
-  const inputtedName = document.querySelector("").value;
-  const inputtedAge = parseInt(document.getElementsByName());
-
+  const inputtedName = document.querySelector("#name").value;
+  const inputtedAge = parseInt(document.querySelector("#value").value);
+  const youngerAge = parseInt(document.querySelector("#youngerAge").value);
+  const olderAge = parseInt(document.querySelector("#olderAge").value);
+  let person = new Person(inputtedName, inputtedAge);
+  person.addMurcuryAge();
+  person .addVenusAge();
+  person.addMarsAge ();
+  person.addJupiterAge ();
   }
 
   function displayDates() {
-    const priceElement = document.getElementById('display-info');
-    priceElement.textContent = ;
+    const displayValue = document.getElementById('results-field');
+    displayValue.textContent = ;
 }
