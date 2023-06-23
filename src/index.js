bc8dc58fc85fa48f09d5ba6a24ca62c99d7972d0
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-23 14:10:02 -0700

    add handle form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import person from './../src/galaxy.js';
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedName = document.querySelector("").value;
+  const inputtedAge = parseInt(document.getElementsByName());
+
+  }
+
+  function displayDates() {
+    const priceElement = document.getElementById('display-info');
+    priceElement.textContent = ;
+}
